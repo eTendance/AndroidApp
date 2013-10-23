0c05cdea7badafb8d1bb7ebd99c1c086ba00afd1
Courtney Redlinger <credlinger@smu.edu>
2013-10-23 17:54:53 -0500

    Added functionality to sign-in screen
    pulls entered username and password, displays for testing purposes

--- eTendance/gen/com/example/etendance/R.java
+++ eTendance/gen/com/example/etendance/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int password=0x7f080001;
+        public static final int sign_in=0x7f080002;
+        public static final int username=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,6 +40,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int etendance=0x7f050002;
         public static final int log_in=0x7f050003;
+        public static final int login_dialog_test=0x7f050006;
         public static final int pass_hint=0x7f050005;
         public static final int user_hint=0x7f050004;
     }
--- eTendance/src/com/example/etendance/MainActivity.java
+++ eTendance/src/com/example/etendance/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.etendance;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +18,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //initialize views
+        final EditText user = (EditText)findViewById(R.id.username);
+        final EditText pass = (EditText)findViewById(R.id.password);
+        Button sign_in = (Button)findViewById(R.id.sign_in);
+        
+        //set onClickListener for button
+        sign_in.setOnClickListener(new OnClickListener() {
+        	
+        	@Override
+        	public void onClick(View v) {
+        		String username = user.getText().toString();
+        		String password = pass.getText().toString();
+        		showLoginInfo(username, password);
+        		
+        		user.setText("");
+        		pass.setText("");
+        	}
+        });
     }
 
 
@@ -18,6 +45,18 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    public void showLoginInfo (String username, String password) {
+    	Context context = getApplicationContext();
+    	int duration = Toast.LENGTH_LONG;
+    	
+    	CharSequence text = "User: " + username + " Password: " + password;
+    	
+    	
+    	Toast loginToast = Toast.makeText(context, text, duration);
+    	loginToast.show();
     }
     
 }
